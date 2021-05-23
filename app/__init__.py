b46171c620285bbe3971c824c2439feccb897755
Stalin Sabu Thomas <44336699+legendof-selda@users.noreply.github.com>
2021-05-24 03:06:11 +0530

    connected to mysql

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,9 @@ from app import DB
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = CONNECTION_STRING
 app.config['SECRET_KEY'] = SECRET_KEY
-
 # DB.init_app(app)
 db = SQLAlchemy(app)
-db.create_all()
+# db.create_all()
 
 from app import controller
 
--- app/model/HeroClass.py
+++ app/model/HeroClass.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class HeroClass(db.Model):
-    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
-    name = db.Column(db.String(80), nullable=False)
-    max_hp = db.Column(db.Float, nullable=False, default=100)
-    max_manna = db.Column(db.Float, nullable=False, default=100)
+    id = db.Column("hero_class_id", db.Integer, primary_key=True, auto_increment=True)
+    name = db.Column("class_type", db.String(80), nullable=False)
+    max_hp = db.Column("max_hp", db.Float, nullable=False, default=100)
+    max_manna = db.Column("max_manna", db.Float, nullable=False, default=100)
